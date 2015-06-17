773b5bca6bce3eefcb9b7c753b4075e069690a4d
Bekk Blando <bekkblando@gmail.com>
2015-06-16 20:47:31 -0400

    iPython may be new MVP

--- textminer/separator.py
+++ textminer/separator.py
@@ -1,3 +1,11 @@
+import re
+
+
+def words(test):
+    if len(re.findall(r"[a-zA-Z]", test)) > 0:
+        return test.split()
+
+
 def date(test):
     pass
 
@@ -7,12 +15,20 @@ def zipcode(test):
 
 
 def money(test):
-    pass
+    if len(test) < 2:
+        return None
+    if re.fullmatch(r"\$.*\.\d{1}|\$.*\.\d{3}|\${2}\d.*|\$\d*,\d{1,2}|\d+", test):
+        return None
+    else:
+        cleantext = ''.join((re.findall(r"[\d\.]", test)))
+        print(cleantext)
+        return{"currency": "$", "amount": float(cleantext)}
 
 
 def phone_number(test):
-    pass
-
-
-def words(test):
-    pass
+    if len(re.findall(r"[\d]", test)) == 10:
+        cleantext = ''.join((re.findall(r"[\d]", test)))
+        print(cleantext)
+        area = re.findall(r"^\d{3}", cleantext)
+        number = cleantext[3:6] + '-' + cleantext[6:]
+        return {"area_code": ''.join(area), "number": number}
